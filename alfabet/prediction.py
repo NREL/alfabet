b30a875059d86104bdf2be318852f21047ba5f34
fthibaud <fthibaud95@gmail.com>
2024-05-03 10:46:23 +0200

    Fix tensorflow import

--- alfabet/prediction.py
+++ alfabet/prediction.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Tuple
 import numpy as np
 import pandas as pd
 import pooch
-from nfp.frameworks import tf
+import tensorflow as tf
 from pooch import retrieve
 from rdkit import RDLogger
 
